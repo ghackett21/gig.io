0fdf952a7ff0767254c67862ec714b3b823411d5
Samuel P. Fellers <sfellers@xinu12.cs.purdue.edu>
2017-02-07 09:55:56 -0500

    Added dummy post enumeration

--- Public/app.js
+++ Public/app.js
@@ -1,6 +1,28 @@
 var app = angular.module("myApp", []);
 
-app.controller("Controller", function($scope) {
+app.controller("postController", function($scope) {
+
+$scope.postsData  = [
+        {
+            username: "sfellers",
+           	description: "rake my yard",
+            bid: 8.00,
+            location: "872 West Penn St."
+        },
+        {
+            username: "jdoe",
+           	description: "take photos of my dog",
+            bid: 19.50,
+            location: "1600 N Elm Ave."
+        },
+        {
+            username: "dtrump",
+           	description: "build a wall",
+            bid: 290.00,
+            location: "2980 Mexico St."
+        }
+    ];
+
 
 
 });
