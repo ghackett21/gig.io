4249d57a88a009e58f7a46f59f50821f6ed13693
Samuel P. Fellers <sfellers@xinu12.cs.purdue.edu>
2017-02-07 10:16:00 -0500

    Adding Profile Picture

--- Public/app.js
+++ Public/app.js
@@ -2,6 +2,8 @@ var app = angular.module("myApp", []);
 
 app.controller("postController", function($scope) {
 
+$scope.img = "http://howtodrawdat.com/wp-content/uploads/2014/03/1stpic-lucky-charms-leprechaun.png";
+
 $scope.postsData  = [
         {
             username: "sfellers",
@@ -23,7 +25,36 @@ $scope.postsData  = [
         }
     ];
 
+$scope.usersData  = [
+        {
+            username: "sfellers",
+           	description: "im sam.",
+            avg_rating: 3.23,
+			num_ratings: 23,
+            location: "839 S Albany Ave.",
+			profile_img: "http://howtodrawdat.com/wp-content/uploads/2014/03/1stpic-lucky-charms-leprechaun.png"
+        },
+        {
+            username: "sfellers1",
+           	description: "im sam.",
+            avg_rating: 3.23,
+			num_ratings: 23,
+            location: "839 S Albany Ave.",
+			profile_img: "http://howtodrawdat.com/wp-content/uploads/2014/03/1stpic-lucky-charms-leprechaun.png"
+        },
+        {
+            username: "sfellers2",
+           	description: "im sam.",
+            avg_rating: 3.23,
+			num_ratings: 23,
+            location: "839 S Albany Ave.",
+			profile_img: "http://howtodrawdat.com/wp-content/uploads/2014/03/1stpic-lucky-charms-leprechaun.png"
+        }
+    ];
 
+$scope.getUserPic = function(username) {
+	console.log("username is :" + username);
+};
 
 });
 
