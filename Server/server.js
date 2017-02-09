ea34f2679bc4af6f110d53b02a7cec3fb18800f0
Sam Fellers <sfellers@purdue.edu>
2017-02-09 17:58:19 -0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 	host     : 3306,
 	user     : 'root',
 	password : 'password',
-	database : 'gigio'
+	database : 'sfellers'
 });
 
 /* connect to database */
