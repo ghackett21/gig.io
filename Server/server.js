735ccb1852999149bcfca95d9ffc1b534f7b517d
ghackett21 <ghackett@purdue.edu>
2017-02-28 20:02:13 -0500

    set port back to 8081

--- Server/server.js
+++ Server/server.js
@@ -709,7 +709,7 @@ function GetDate() {
 }
 
 /* start express server */
-var server = app.listen(3000, function() {
+var server = app.listen(8081, function() {
 	var host = server.address().address;
 	var post = server.address().port;
 })
