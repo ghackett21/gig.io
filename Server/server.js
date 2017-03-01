7676c1fd612f0d685d2f010f2d83b4f255b1eb97
ghackett21 <ghackett@purdue.edu>
2017-03-01 12:10:42 -0500

    Delete Post

--- Server/server.js
+++ Server/server.js
@@ -646,6 +646,42 @@ function GetUser_Helper(userId) {
  	});
  } 
 
+/**
+* Delete post based on postId
+* Accepts: postId
+* Returns: State
+*/ 
+app.post("/DeletePost", function(req, res) {
+	console.log("DeletePost");
+
+	var callback = function(result) {
+		if (result < 0) {
+			res.json({"Response": "DeletePost failed", "State": result });
+		}
+		else {
+			res.json({"Response": "DeletePost successful", "State": result });
+		}
+	}
+
+	DeletePost(req.body.postId, callback);
+});
+
+function DeletePost(postId, callback) {
+	console.log("DeletePost: postId: " + postId);
+
+	var delete = "DELETE FROM Posting WHERE Pid = " + postId;
+
+	connection.query(delete, function(err, rows) {
+		if (err) {
+			console.log("DeletePost: database error, " + err);
+			return callback(-2);
+		}
+		else {
+			return callback(0);
+		}
+	}
+}
+
 
 /** 
  * Place a bid on a post
