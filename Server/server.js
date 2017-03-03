c8d4a5c97d3fac3ed693b5e524da5242f2048107
ghackett21 <ghackett@purdue.edu>
2017-03-03 14:00:53 -0500

    seperated login

--- Server/server.js
+++ Server/server.js
@@ -12,6 +12,7 @@ var getDate = require('./helpers/getDate');
 var connection = require('./helpers/connection');
 
 /* users */
+var login = require('./users/login');
 var updateProfile = require('./users/updateProfile');
 var getUser = require('./users/getUser');
 
@@ -136,21 +137,7 @@ passport.use(new LocalStrategy(
 ));
 
 app.post('/login', passport.authenticate('local', { failureRedirect: '/login'}), function(req, res) {
-
-	console.log("sending post login req = %j", req.user);
-	//res.json({"status": 200, "redirect" : "/index.html"});
-	req.logIn(req.user, function(err) {
-			
-			if (err) {
-				   return res.status(500).json({
-				       err: 'Could not log in user'
-				   });
-			}
-			console.log("is auth? + "  + req.isAuthenticated()); 
-			req.session.save(() => {
-				res.json({"status": 200, "redirect" : "/index.html"});
-    		})
-	});	
+	login(req, res);
 });
 
 
@@ -174,38 +161,6 @@ app.post('/logout', function(req, res) {
   req.logout();
   res.json({"redirect": '/login.html'});
 });
-
-
-/**
- * Search the database for a User with the given username and password,
- * if exactly one user matches, return success.
- */ 
-function Login(username, password, callback) {
-	console.log("Login: ", username, password);
-	var select = "SELECT * FROM Users WHERE Username LIKE '" + username + "'";
-	connection.query(select, function(err, rows) {
-		if (err) {
-			/* an error occured */
-			console.log("Login Failed");
-			return callback(-2);
-		}
-		else {
-			if (rows.length == 1) {
-					console.log("password = %s, hash = %s\n", password, rows[0].Password);
-				if(bcrypt.compareSync(password, rows[0].Password)){
-					console.log("Login Successful");
-					return callback(0);
-				}else{
-					console.log("Login Failed: Bad Pass");
-					return callback(-3);
-				}
-			}
-			else {
-				return callback(-2);
-			}
-		}
-	});
-}
 
 
 /**
---
+++ Server/users/login.js
@@ -0,0 +1,48 @@
+module.exports =  function(req, res) {
+
+	console.log("sending post login req = %j", req.user);
+	//res.json({"status": 200, "redirect" : "/index.html"});
+	req.logIn(req.user, function(err) {
+			
+			if (err) {
+				   return res.status(500).json({
+				       err: 'Could not log in user'
+				   });
+			}
+			console.log("is auth? + "  + req.isAuthenticated()); 
+			req.session.save(() => {
+				res.json({"status": 200, "redirect" : "/index.html"});
+    		})
+	});	
+}
+
+/**
+ * Search the database for a User with the given username and password,
+ * if exactly one user matches, return success.
+ */ 
+function login(username, password, callback) {
+	console.log("Login: ", username, password);
+	var select = "SELECT * FROM Users WHERE Username LIKE '" + username + "'";
+	connection.query(select, function(err, rows) {
+		if (err) {
+			/* an error occured */
+			console.log("Login Failed");
+			return callback(-2);
+		}
+		else {
+			if (rows.length == 1) {
+					console.log("password = %s, hash = %s\n", password, rows[0].Password);
+				if(bcrypt.compareSync(password, rows[0].Password)){
+					console.log("Login Successful");
+					return callback(0);
+				}else{
+					console.log("Login Failed: Bad Pass");
+					return callback(-3);
+				}
+			}
+			else {
+				return callback(-2);
+			}
+		}
+	});
+}
