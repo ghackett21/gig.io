2430f865d69585d0b7f8bc8287f807b388db4a77
ghackett21 <ghackett@purdue.edu>
2017-03-03 14:03:41 -0500

    login test

--- Server/users/login.js
+++ Server/users/login.js
@@ -20,29 +20,29 @@ module.exports =  function(req, res) {
  * Search the database for a User with the given username and password,
  * if exactly one user matches, return success.
  */ 
-function login(username, password, callback) {
-	console.log("Login: ", username, password);
-	var select = "SELECT * FROM Users WHERE Username LIKE '" + username + "'";
-	connection.query(select, function(err, rows) {
-		if (err) {
-			/* an error occured */
-			console.log("Login Failed");
-			return callback(-2);
-		}
-		else {
-			if (rows.length == 1) {
-					console.log("password = %s, hash = %s\n", password, rows[0].Password);
-				if(bcrypt.compareSync(password, rows[0].Password)){
-					console.log("Login Successful");
-					return callback(0);
-				}else{
-					console.log("Login Failed: Bad Pass");
-					return callback(-3);
-				}
-			}
-			else {
-				return callback(-2);
-			}
-		}
-	});
-}
+//function login(username, password, callback) {
+//	console.log("Login: ", username, password);
+//	var select = "SELECT * FROM Users WHERE Username LIKE '" + username + "'";
+//	connection.query(select, function(err, rows) {
+//		if (err) {
+//			/* an error occured */
+//			console.log("Login Failed");
+//			return callback(-2);
+//		}
+//		else {
+//			if (rows.length == 1) {
+//					console.log("password = %s, hash = %s\n", password, rows[0].Password);
+//				if(bcrypt.compareSync(password, rows[0].Password)){
+//					console.log("Login Successful");
+//					return callback(0);
+//				}else{
+//					console.log("Login Failed: Bad Pass");
+//					return callback(-3);
+//				}
+//			}
+//			else {
+//				return callback(-2);
+//			}
+//		}
+//	});
+//}
