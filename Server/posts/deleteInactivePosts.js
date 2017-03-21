823d5c18c406827c257982de942401b7767c1193
ghackett21 <ghackett@purdue.edu>
2017-03-21 18:20:39 -0400

    created deleteInactivePosts

---
+++ Server/posts/deleteInactivePosts.js
@@ -0,0 +1,13 @@
+/**
+ * automatic inactive post deletion 
+ * delete any and all posts that have been inactive for 30 or more days
+ */
+
+module.exports = function() {
+	console.log("Delete Inactive Posts");
+
+	/* find newest bid on all active posts and delete ones that
+	 occured more than 30 days ago */
+	 
+	 
+}	
