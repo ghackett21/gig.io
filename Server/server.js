ceb1b62ee7f2594055e2452f0db9ccc643fbbf42
ghackett21 <ghackett@purdue.edu>
2017-03-31 01:52:38 -0400

    require auth for user post

--- Server/server.js
+++ Server/server.js
@@ -131,6 +131,10 @@ app.get('/makepost.html', ensureAuthenticated, function(req, res) {
     res.sendFile(__dirname + '/public/makepost.html');
 });
 
+app.get('/userPost.html', ensureAuthenticated, function(req, res) {
+    res.sendFile(__dirname + '/public/userPost.html');
+});
+
 app.get('/profile.html', ensureAuthenticated, function(req, res) {
 	console.log("dir = " + __dirname);
     res.sendFile(__dirname + '/public/profile.html');
