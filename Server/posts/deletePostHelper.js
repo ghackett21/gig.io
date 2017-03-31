b86232be333e22e8c8530b3ac64b8a3e293df13c
ghackett21 <ghackett@purdue.edu>
2017-03-30 20:46:01 -0400

    delete bids along with post

--- Server/posts/deletePostHelper.js
+++ Server/posts/deletePostHelper.js
@@ -11,9 +11,21 @@ module.exports = function(postId, callback) {
 			}
 		}
 		else {
-			if (callback) {
-				return callback(0);
-			}
+			/* delete associated bids from bid table */
+			var deleteBids = "DELETE FROM Bids WHERE Pid="  + postID;
+			connection.query(deleteBids, function(err, rows) {
+				if (err) {
+					console.log("DeletePost: database error when deleting bids.", err);
+					if (callback) {
+						return callback(-2);
+					}
+				}
+				else {
+					if (callback) {
+						return callback(0);
+					}
+				}
+			});
 		}
 	});
 }
