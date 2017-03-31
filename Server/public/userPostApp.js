b278a031901d5cac55abeaf39a5dfcd53f95125f
ghackett21 <ghackett@purdue.edu>
2017-03-31 11:15:36 -0400

    typo fix

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -454,7 +454,7 @@ $scope.sortByLowestBid = function() {
                             /* set bid onlick() */
                             var bidRows = document.getElementById("bid-table").rows;
 
-                            for var j = 0; j < bidRows.length; j++) {
+                            for (var j = 0; j < bidRows.length; j++) {
                                 bidRows[i].onclick() = function() {
                                     console.log("bid " + i + " clicked!");
                                 }
