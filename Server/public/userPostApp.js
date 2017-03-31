b2f1a59b65c1de5b63a3882732515c6736585952
ghackett21 <ghackett@purdue.edu>
2017-03-31 11:28:16 -0400

    continued test of bid selection

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -132,6 +132,9 @@ app.controller("userPostController", [ '$scope', '$http', function($scope, $http
                                 td[0].innerHTML = date; //bids[i].BidTime;
                                 td[1].innerHTML = bids[i].Username;
                                 td[2].innerHTML = "$" + bids[i].Amount;
+                                clone.onClick = function() {
+                                    console.log("bid " + i + " clicked!");
+                                }
                                 template.parentNode.appendChild(clone);
                             }
                             myMap(myUser.U_Location);
