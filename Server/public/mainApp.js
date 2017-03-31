b5b43c985e6bd963e512d37db16c9370b2b2839f
ghackett21 <ghackett@purdue.edu>
2017-03-31 10:39:02 -0400

    clear text box after bid

--- Server/public/mainApp.js
+++ Server/public/mainApp.js
@@ -816,6 +816,7 @@ $scope.sortByLowestBid = function() {
 
             // Bid
             $http.post('/Bid', $scope.bid).then(function(response) {
+                $scope.bid.Amount = "";
                 // Update bids displayed
                 var bidData = new Object();
                 bidData.PostId = $scope.pid;
