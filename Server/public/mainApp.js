c31ee79ddd377b71c62dcad531c1d47926491f3d
ghackett21 <ghackett@purdue.edu>
2017-04-03 19:17:16 -0400

    define scope.pid, seperate file for makePostApp

--- Server/public/mainApp.js
+++ Server/public/mainApp.js
@@ -421,6 +421,8 @@ $scope.sortByLowestBid = function() {
                     $scope.phone = post.PhoneNumber;
                     $scope.desc = post.P_Description;
                     $scope.title = post.P_Title;
+                    $scope.Pid = post.Pid;
+
                     $scope.location = post.P_Location;
                     address = post.P_Location;
                     modal.style.display = "block";
@@ -493,65 +495,6 @@ $scope.sortByLowestBid = function() {
         })
     }
 
-}]);
-
-var mylong;
-var mylat;
-app.controller("makePostController", [ '$scope', '$http', function($scope, $http) {
-    $scope.post;
-    $scope.status = "";
-    
-    $scope.makePost = function() {
-        console.log("title = " + $scope.post.title);
-        console.log("description = " + $scope.post.description);
-        console.log("location = " + $scope.post.location);
-        console.log("imageLink = " + $scope.post.imageLink);
-        
-        if($scope.post == undefined){
-            return;
-        }
-
-        if($scope.post == undefined || $scope.post.title == undefined || $scope.post.description == undefined || $scope.post.location == undefined){
-            $scope.status = "Make Sure to fill in all required fields.";
-            return;
-        }
-        /*if($scope.post.imageLink.match((http(s?):)|([/|.|\w|\s])*\.(?:jpg|gif|png) == null){
-            $scope.status = "Please enter a valid imageLink URL";
-            return;
-        }*/
-
-        //NEED TO CONVERT LOCATION TO COORDINATES AND SEND TO DATABASE AS WELL
-        var myloc = $scope.post.location;
-        getCoordinatesForMakePost(myloc);
-       
-    };
-
-    function getCoordinatesForMakePost(location) {
-    console.log("location: " + location);
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-        'address' : location
-        },
-        function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                var myResult = results[0].geometry.location;
-                console.log(myResult.lat() + " , " + myResult.lng());
-                $scope.post.lat = myResult.lat();
-                $scope.post.lng = myResult.lng();
-                console.log("$scope.post: " + $scope.post);
-                $http.post('/CreatePost', $scope.post).then(function(response) {
-                    $scope.post = null;
-                    console.log(response);
-                    if(response.data.State == 0){
-                        $scope.status = "Post successfully created! Don't forget to check for bids.";
-                    }
-                }).catch(function(response) {
-                    console.log("error creating post");
-                });
-           }
-    });
-
-    }
 }]);
 
 function myMap(loc) {
