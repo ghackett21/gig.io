1b344e7f406a9f649408d4ce561ae3dca07f86b4
ghackett21 <ghackett@purdue.edu>
2017-04-03 22:55:56 -0400

    allow for first time bid on post

--- Server/bidding/bid.js
+++ Server/bidding/bid.js
@@ -56,11 +56,15 @@ function bid(userId, postId, amount, callback) {
 						return callback(-2);
 					}
 					else {
-						if (rows[0].Amount < amount) {
+						if (rows.length == 0) {
+							insertBid(userId, postId, amount, callback);
+						}
+						else if (rows[0].Amount < amount) {
 							console.log("Cannot bid more than already bid on same post!");
 							return callback(-4);
 						}
 						else {
+							insertBid(userId, postId, amount, callback);
 
 							var deleteOldBid = "DELETE FROM Bids WHERE Bidid=" + rows[0].Bidid;
 
@@ -69,59 +73,61 @@ function bid(userId, postId, amount, callback) {
 									console.log("Bid: error deleting existing bid!");
 								}
 							});
-
-							var bidTime = getDate();
-							var insert = "INSERT INTO Bids (Uid, Pid, BidTime, Amount) VALUES (" + userId + ", " + postId + ", '" + bidTime + "', " + amount + ")";
-
-
-							connection.query(insert, function(err, rows) {
-								if (err) {
-									console.log("Bid: database error", err);
-									return callback(-2);
-								}
-								else {
-									/* increment the number of bids on the post the bid was for */
-									var updateNumBids = "UPDATE Posting SET NumberOfBids=NumberOfBids+1 WHERE PID=" + postId;
-
-									connection.query(updateNumBids, function(err, rows) {
-										if (err) {
-											console.log("Bid: database error: " + err);
-											return callback(-2);
-										}
-										else {
-											/* check lowest bid and update if needed */
-											var selectLowestBid = "SELECT LowestBid FROM Posting WHERE Pid=" + postId;
-
-											connection.query(selectLowestBid, function(err, rows) {
-												if (err) {
-													console.log("Bid: database error: " + err);
-													return callback(-2);
-												}
-												else {
-													if (rows[0].LowestBid == 0.0 || amount < rows[0].LowestBid) {
-														/* update value in Db */
-														var updateLowestBid = "UPDATE Posting SET LowestBid=" + amount + " WHERE Pid=" + postId;
-
-														connection.query(updateLowestBid, function(err, rows) {
-															if (err) {
-																console.log("Bid: database error: " + err);
-																return callback(-2);
-															}
-															else {
-																return callback(0);
-															}
-														});
-													}
-												}
-											});
-										}
-									});
-								}
-							});
 						}
 					}
 				});
 			}
+		}
+	});
+}
+
+function inserBid(userId, postId, amount, callback) {
+	var bidTime = getDate();
+	var insert = "INSERT INTO Bids (Uid, Pid, BidTime, Amount) VALUES (" + userId + ", " + postId + ", '" + bidTime + "', " + amount + ")";
+
+
+	connection.query(insert, function(err, rows) {
+		if (err) {
+			console.log("Bid: database error", err);
+			return callback(-2);
+		}
+		else {
+			/* increment the number of bids on the post the bid was for */
+			var updateNumBids = "UPDATE Posting SET NumberOfBids=NumberOfBids+1 WHERE PID=" + postId;
+
+			connection.query(updateNumBids, function(err, rows) {
+				if (err) {
+					console.log("Bid: database error: " + err);
+					return callback(-2);
+				}
+				else {
+					/* check lowest bid and update if needed */
+					var selectLowestBid = "SELECT LowestBid FROM Posting WHERE Pid=" + postId;
+
+					connection.query(selectLowestBid, function(err, rows) {
+						if (err) {
+							console.log("Bid: database error: " + err);
+							return callback(-2);
+						}
+						else {
+							if (rows[0].LowestBid == 0.0 || amount < rows[0].LowestBid) {
+								/* update value in Db */
+								var updateLowestBid = "UPDATE Posting SET LowestBid=" + amount + " WHERE Pid=" + postId;
+
+								connection.query(updateLowestBid, function(err, rows) {
+									if (err) {
+										console.log("Bid: database error: " + err);
+										return callback(-2);
+									}
+									else {
+										return callback(0);
+									}
+								});
+							}
+						}
+					});
+				}
+			});
 		}
 	});
 }
