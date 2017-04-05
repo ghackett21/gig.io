bee2cebf9b6c2640174196de20d2d6fa5e2ca015
ghackett21 <ghackett@purdue.edu>
2017-04-05 17:18:32 -0400

    test update profile

--- Server/public/updateProfileApp.js
+++ Server/public/updateProfileApp.js
@@ -1,39 +1,21 @@
 var app = angular.module("myUpdateProfileApp", []);
 
 app.controller("updateProfileController", [ '$scope', '$http', function($scope, $http) {
-    $scope.testAuth = function() {
-    		$http.post('/protected').then(function(response) {
-    			console.log("response = %j", response);
-    			//window.location = response.data.redirect;
-    		});
-    };
+    $scope.user;
+
+    $scope.register = function() {
+        console.log("email = " + $scope.user.email);
+        console.log("description = " + $scope.user.description);
+        console.log("image link = " + $scope.user.image);
+        console.log("phone number = " + $scope.user.phone);
+
+    }
+
 	$scope.logout = function() {
 		$http.post('/logout').then(function(response) {
 			console.log("response = %j", response);
 			window.location = response.data.redirect;
 		});
 	};
-    window.onload = function() {
-    	$http.post('/GetUser').then(function(response) {
-    	    console.log(response.data.Result[0]);
-     	    var user = response.data.Result[0];
-     	    var template = document.querySelector('#tmplt');
-     	    var clone = template.content.cloneNode(true);
-     	    var h1 = clone.querySelectorAll('h1');
-            h1[0].innerHTML = user.Username;
-            var h3 = clone.querySelectorAll('h3');
-            h3[0].innerHTML = user.U_Description;
-            h3[1].innerHTML = 'Post Rating: ' + user.AVG_PostRate + '/5.0';
-            h3[2].innerHTML = 'Bid Rating ' + user.AVG_BidRate + '/5.0';
-            h3[3].innerHTML = user.PhoneNumber;
-            h3[4].innerHTML = user.EmailAddress;
 
-            /*var rating = user.AverageRating;
-
-            for(var i = 0; i < rating; i++) {
-                AddImage();
-            }*/
-            template.parentNode.appendChild(clone);
-        })
-    };
 }]);
