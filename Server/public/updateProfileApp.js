09812692ccc44fe7e653ad8cc4bdb07525498a6f
ghackett21 <ghackett@purdue.edu>
2017-04-06 19:59:06 -0400

    added update location

--- Server/public/updateProfileApp.js
+++ Server/public/updateProfileApp.js
@@ -9,17 +9,54 @@ app.controller("updateProfileController", [ '$scope', '$http', function($scope, 
         console.log("description = " + $scope.user.description);
         console.log("image link = " + $scope.user.image);
         console.log("phone number = " + $scope.user.phone);
+        console.log("location = " + $scope.user.location);
 
-        /* make register request */
-        $http.post('/UpdateProfile', $scope.user).then(function(response) {
-            $scope.user = null;
-            console.log(response);
-            if(response.data.State == 0){
-                $scope.status = "Update Successful!";
-            }
-        }).catch(function(response) {
-                    console.log("error updating profile");
-        });
+        if (user.location == undefined) {
+            /* make register request */
+            $http.post('/UpdateProfile', $scope.user).then(function(response) {
+                $scope.user = null;
+                console.log(response);
+                if(response.data.State == 0){
+                    $scope.status = "Update Successful!";
+                }
+            }).catch(function(response) {
+                        console.log("error updating profile");
+            });
+        }
+        else {
+            getCoordinatesForUpdateProfile($scope.user.location);
+        }
+    }
+
+    function getCoordinatesForUpdateProfile(location) {
+    console.log("location: " + location);
+    /* get coordinates */
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'address' : location
+    },
+    function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var myResult = results[0].geometry.location;
+            console.log(myResult.lat() + " , " + myResult.lng());
+            $scope.user.lat = myResult.lat();
+            $scope.user.lng = myResult.lng();
+            console.log("$scope.user: " + $scope.user);
+
+            /* make register request */
+            $http.post('/UpdateProfile', $scope.user).then(function(response) {
+                $scope.user = null;
+                console.log(response);
+                if(response.data.State == 0){
+                    $scope.status = "Update Successful!";
+                }
+            }).catch(function(response) {
+                        console.log("error updating profile");
+            });
+        }
+       
+    });
+
     }
 
 	$scope.logout = function() {
