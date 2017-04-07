47f89b400923367c9e96d7fbaccd5fea33372574
ghackett21 <ghackett@purdue.edu>
2017-04-07 12:15:21 -0400

    added parens

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -459,7 +459,7 @@ $scope.sortByLowestBid = function() {
     }
 }]);
 
-function acceptBid {
+function acceptBid() {
     console.log("Accept clicked");
     var bidid = $(this).parentElement.id;
     console.log("bidid = " + bidid);
