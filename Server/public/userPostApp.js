91a8f0afe8c6fb1d4560cc6930c97456593f9af9
ghackett21 <ghackett@purdue.edu>
2017-04-07 00:08:05 -0400

    Revert "set onclick for accept buttons"
    This reverts commit 0aaef4bc97ae6b50a501a7f851e6ae72ab16331d.

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -428,7 +428,6 @@ $scope.sortByLowestBid = function() {
                 td[2].innerHTML = amountString
                 td[3].innerHTML = bids[i].AVG_BidRate + "/5";
                 td[4].id = bids[i].Bidid;
-                td[4].childElement.children.onClick = acceptBid();
                 template.parentNode.appendChild(clone);
             }
             /* call display map function */
