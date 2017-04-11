024a0e31c4e66bcd4146d9f7d7704af93dda58fa
ghackett21 <ghackett@purdue.edu>
2017-04-11 18:22:30 -0400

    another attempt

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -502,7 +502,7 @@ $scope.sortByLowestBid = function() {
                 td[2].innerHTML = amountString
                 td[3].innerHTML = bids[i].AVG_BidRate + "/5";
                 td[4].id = bids[i].Bidid;
-                $compile(td)($scope);
+                $compile(td[4])($scope);
                 template.parentNode.appendChild(clone);
 
             }
