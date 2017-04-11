7f90e2278b12ab572a33c27947b3deae3af8dd51
ghackett21 <ghackett@purdue.edu>
2017-04-11 14:46:44 -0400

    fixed close post

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -514,8 +514,8 @@ $scope.sortByLowestBid = function() {
 
         /* close post */
         $http.post('/ClosePost', bid).then(function(response) {
+           location.reload(true);
            console.log("Close Post: " + bid.PostId + ", amount: " + bid.Amount);
-           //location.reload(true);
         }).catch(function(response) {
             console.log("error in Close Post");
         })
