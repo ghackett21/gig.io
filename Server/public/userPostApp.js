6594f5e5770c5e1e7e7e144bacfe4bb9b71de994
ghackett21 <ghackett@purdue.edu>
2017-04-11 18:35:20 -0400

    compile element before appending

--- Server/public/userPostApp.js
+++ Server/public/userPostApp.js
@@ -502,7 +502,8 @@ $scope.sortByLowestBid = function() {
                 td[2].innerHTML = amountString
                 td[3].innerHTML = bids[i].AVG_BidRate + "/5";
                 td[4].id = bids[i].Bidid;
-                template.parentNode.appendChild(clone);
+                var compiledElement = $compile(clone)($scope);
+                template.parentNode.appendChild(compiledElement);
 
             }
             /* call display map function */
