5cf46ce0a527beb6c9677d00e8425fba2c9dcd8a
ghackett21 <ghackett@purdue.edu>
2017-04-13 19:54:43 -0400

    forgot to commit file

--- Server/posts/getWonPosts.js
+++ Server/posts/getWonPosts.js
@@ -21,6 +21,7 @@ module.exports = function(req, res) {
 }
 
 function getWonPosts(userId, callback) {
+  	var select = "SELECT Posting.P_Title, Posting.Pid, Posting.P_Location, Posting.P_Lat, Posting.P_Long, Posting.CreationTime, Posting.P_Description, Posting.NumberOfBids, Posting.LowestBid, Posting.P_Image, Users.Uid, Users.Username, Users.U_Location, Users.PhoneNumber, Users.DateJoined, Users.EmailAddress, Users.AVG_PostRate, Users.AVG_BidRate FROM Posting Inner Join Users On Posting.Uid=Users.Uid WHERE Posting.Winning_Uid=" + userId;
 
   	connection.query(select, function(err, rows) {
   		if (err) {
