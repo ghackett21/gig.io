9d91308ffb42c1ee3808cd5a2158d355b6b7468e
ghackett21 <ghackett@purdue.edu>
2017-04-17 13:51:49 -0400

    changed getBidsHelper to allow completed status

--- Server/bidding/getBidsHelper.js
+++ Server/bidding/getBidsHelper.js
@@ -26,7 +26,7 @@ module.exports = function (postId, callback) {
 					}
 				}); 
 			}
-			else if (rows[0].Status == 1) {
+			else {
 				console.log("Winning bidid: " + rows[0].Winning_Bidid);
 				/* if the post is pending, only return info of the winning bid */
 				var selectBids = "SELECT Bids.Bidid, Bids.Uid, Bids.Pid, Bids.BidTime, Bids.Amount, Users.Username, Users.AVG_BidRate FROM Bids Inner Join Users On Bids.Uid=Users.Uid WHERE Bids.Bidid Like '" + rows[0].Winning_Bidid + "'";
