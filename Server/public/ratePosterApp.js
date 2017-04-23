3e54107b2180f527fb2e6968a3d3d3f64f0233c1
ghackett21 <ghackett@purdue.edu>
2017-04-23 16:38:28 -0400

    seperate pages for rating bidder and poster

---
+++ Server/public/ratePosterApp.js
@@ -0,0 +1,43 @@
+/**
+ * makePostApp.js
+ * This file is the controller for the make post page
+ */
+
+var app = angular.module("myApp", []);
+
+app.controller("rateController", [ '$scope', '$http', function($scope, $http){
+    $scope.rating;
+    $scope.status = "";
+    $scope.rate = function() {
+        console.log("score = " + $scope.rating.score);
+        console.log("description = " + $scope.rating.description);
+        
+        if($scope.rate == undefined){
+            return;
+        }
+
+        /* ensure all fields have been filled out before submitting */
+        if($scope.rating == undefined || $scope.rating.score == undefined || $scope.rating.description == undefined){
+            $scope.status = "Make Sure to fill in all required fields.";
+            return;
+        }
+
+
+        var rate_req = {"comment":$scope.rating.description, "userId":localStorage.getItem("bidder_userId"), "ratingValue":$scope.rating.score, "postId":localStorage.getItem("postId"), "ratingType":"Bid"};
+
+        console.log("rating: " + rate_req);
+
+        $http.post('/CreateRating', rate_req).then(function(response) {
+                    $scope.rate = null;
+                    console.log(response);
+                    if(response.data.State == 0){
+                        $scope.status = "Rating submitted successfully. Thank you for your feedback on this user.";
+                    }
+                }).catch(function(response) {
+                    console.log("error submitting rating");
+                });
+       
+    };
+    
+
+}]);
