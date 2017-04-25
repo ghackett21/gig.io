40b2c4ca788aefc5d0a4d0f0bd349fdd8bc16338
ahouvena <ahouvena@purdue.edu>
2017-04-25 17:45:05 -0400

    Starting integrating payments

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,26 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
+var dwolla = require('dwolla-v2');
+
+/* Dwolla keys */
+var appKey = "b36FS5lRd58OASDZ6E9eMH9Cej5SgC63THvHDkgVVDC4czmBDG";
+var secretKey = "8gu99HObZ61VZt8Mz7CGEQyK42BcedKDsjO5I7Y0D493uSPfTf";
+
+/* Dwolla client */
+var client = new dwolla.Client({
+    key: appKey, 
+    secret: secretKey,
+    environment: 'sandbox'
+});
+
+/* Dwolla access token */
+// TODO - this expires after 60 minutes
+var appToken = new client.Token({ 
+    access_token: 'PU7z2S5ZY64shIxxfPmNbVB1ByumG5Qn3KzSQ7aBq5TZnAThOB', 
+    refresh_token: ""
+});
+
 
 var transporter = nodemailer.createTransport({
         service: 'Gmail',
