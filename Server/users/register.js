1ea6f63b93e5080c4e12c40ce1cf5c935aca5580
ghackett21 <ghackett@purdue.edu>
2017-04-26 14:20:26 -0400

    fix typo

--- Server/users/register.js
+++ Server/users/register.js
@@ -66,7 +66,7 @@ function register(user, callback) {
 						return callback(-2);
 					}
 					else {
-						var selectUserId = "SELECT Uid from USERS WHERER Username=" + connection.escape(user.username) + " AND Password='" + hash + "'";
+						var selectUserId = "SELECT Uid from USERS WHERE Username=" + connection.escape(user.username) + " AND Password='" + hash + "'";
 
 						connection.query(selectUserId, function(err, rows) {
 							if (err) {
