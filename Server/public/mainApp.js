b72595b2d7062ceed16e1790cde060573f861503
ghackett21 <ghackett@purdue.edu>
2017-04-26 19:13:37 -0400

    clear pending posts when modal opened

--- Server/public/mainApp.js
+++ Server/public/mainApp.js
@@ -914,7 +914,11 @@ app.controller("adminController", ['$scope', '$http', function($scope, $http) {
 				document.getElementById('reportHistory').style.display = "none";
 				document.getElementById('noReports').style.display = "block";
 			}
-        })
+        });
+
+        $http.post('/ClearPendingReports', format).then(function(reponse) {
+            console.log("Pending reports cleared");
+        });
 		console.log("hello " + uid);
 		var modal = document.getElementById('myModal');
 
