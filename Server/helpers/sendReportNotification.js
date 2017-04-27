92077d8fb7c112f5f809ffc3ad25e8540419c582
Samuel P. Fellers <sfellers@borg19.cs.purdue.edu>
2017-04-27 18:13:24 -0400

    notification report

---
+++ Server/helpers/sendReportNotification.js
@@ -0,0 +1,39 @@
+var nodemailer = require('nodemailer');
+/**
+ * Send Notification to User
+ * Params <Email to send to, Message to Send, Subject>
+ */
+
+module.exports = function(reportee, type, callback) {
+	//post title and winning bid
+	//posters username
+	var msg, email;
+	msg = "Hello, " + reportee.Username +", We are contacting you to let you know that a report was recently filed against you.\nThe given reason was : " + type + ".\n The case will be reviewed shortly.\nBest\n- Gig.io Team\n";
+	email = reportee.EmailAddress;
+
+	var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'gigdotio@gmail.com',
+            pass: 'geofffff'
+        }
+    });
+
+	console.log(msg);
+	var mailOptions = {
+		from: 'gigdotio@gmail.com', // sender address
+		to: email, 					// list of receivers
+		subject: "Gig.io Report Notification", 
+		text: msg
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+		    return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
+		callback(1);
+	});
+
+	
+}
