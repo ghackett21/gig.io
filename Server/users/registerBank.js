1eea94c75309701d4a7cc029403e1f655b5b3afe
ahouvena <ahouvena@purdue.edu>
2017-04-27 12:33:52 -0400

    formatting

--- Server/users/registerBank.js
+++ Server/users/registerBank.js
@@ -35,78 +35,59 @@ module.exports = function(req, res) {
  */
 function registerBank(user, callback) {
 	
-	console.log("registerBank: ", user.routingNum, user.accountNum, user.accountType, user.name);
-	
-	/* check for existing user with username */
-/*	var select = "SELECT * FROM Users WHERE username LIKE " + connection.escape(user.username);
+    console.log("registerBank: ", user.routingNum, user.accountNum, user.accountType, user.name);
 
-	connection.query(select, function(err, rows) {
-		if (err) {
-			console.log("registerBank Failed: ", err);
-			return callback(-2);
-		}
-		else {
-*/
-			/* if user with username already exists... */
-			/*if (rows.length > 0) {
-				console.log("user exists")
-				return callback(-3);
-			}*/
-			//else {
-                // Send account info to dwolla
-                var requestBody = {
-                    routingNumber: user.routingNum,
-                    accountNumber: user.accountNum,
-                    type: user.accountType,
-                    name: user.name
-                };
+    // Send account info to dwolla
+    var requestBody = {
+        routingNumber: user.routingNum,
+        accountNumber: user.accountNum,
+        type: user.accountType,
+        name: user.name
+    };
 
-                var requestURL = "https://api-sandbox.dwolla.com/customers/" + user.dwollaID.dwollaUID + "/funding-sources"; 
+    var requestURL = "https://api-sandbox.dwolla.com/customers/" + user.dwollaID.dwollaUID + "/funding-sources"; 
 
-                appToken.post(requestURL, requestBody).then(function(res) {
-                    var paySourceID = res.headers.get('location');
-                    var fundingURL = paySourceID;
-                    paySourceID = paySourceID.replace("https://api-sandbox.dwolla.com/funding-sources/", "");
+    appToken.post(requestURL, requestBody).then(function(res) {
+        var paySourceID = res.headers.get('location');
+        var fundingURL = paySourceID;
+        paySourceID = paySourceID.replace("https://api-sandbox.dwolla.com/funding-sources/", "");
 
-                    console.log(paySourceID);
+        console.log(paySourceID);
                    
-                    // Initiate micro-deposits
-                    var microDepositURL = fundingURL + "/micro-deposits";
-                    console.log(microDepositURL);
-                    appToken.post(microDepositURL).then(function(res) {
-                       console.log('initiated micro-deposits');
-                       // Verify micro-deposits
-                       var requestBody = {
-                            amount1: {
-                                value: '0.03',
-                                currency: 'USD'
-                            },
-                            amount2: {
-                                value: '0.09',
-                                currency: 'USD'
-                            }
-                       };
-                       appToken.post(microDepositURL, requestBody);
-                    });
+        // Initiate micro-deposits
+        var microDepositURL = fundingURL + "/micro-deposits";
+        console.log(microDepositURL);
+        appToken.post(microDepositURL).then(function(res) {
+            console.log('initiated micro-deposits');
+        
+        // Verify micro-deposits
+            var requestBody = {
+                amount1: {
+                    value: '0.03',
+                    currency: 'USD'
+                },
+                amount2: {
+                    value: '0.09',
+                    currency: 'USD'
+                }
+            };
+            appToken.post(microDepositURL, requestBody);
+        });
  
 
-                    var insert = "UPDATE Users SET dwollaPaySourceID=" + connection.escape(paySourceID) + " WHERE dwollaUID=" + connection.escape(user.dwollaID.dwollaUID);
-                    console.log(insert);
+        var insert = "UPDATE Users SET dwollaPaySourceID=" + connection.escape(paySourceID) + " WHERE dwollaUID=" + connection.escape(user.dwollaID.dwollaUID);
+        console.log(insert);
 
-				    connection.query(insert, function(err, rows) {
-					    if (err) {
-						    /* an error occured */
-						    console.log("RegisterBank Failed", err);
-						    return callback(-2);
-					    }
-					    else {
-						    console.log("RegisterBank Successful");
-						    return callback(0);
-
-					    }
-				    });
-               });
-			//}
-//		}
-	//});
+		connection.query(insert, function(err, rows) {
+			if (err) {
+				/* an error occured */
+				console.log("RegisterBank Failed", err);
+				return callback(-2);
+			}
+			else {
+				console.log("RegisterBank Successful");
+				return callback(0);
+			}
+		});
+    });
 }
