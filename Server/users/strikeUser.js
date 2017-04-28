e0b79d8b9d763e5d2315c65b1f0513e99625af41
ghackett21 <ghackett@purdue.edu>
2017-04-28 01:24:15 -0400

    strikeuser checks report state

--- Server/users/strikeUser.js
+++ Server/users/strikeUser.js
@@ -31,43 +31,58 @@ module.exports = function(req, res) {
 }
 
 function strikeUser(userId, reportId, callback) {
-	var selectStrikes = "SELECT NumberOfStrikes FROM Users WHERE Uid=" + userId;
+	/* make sure that the report state is 1 */
+	var selectReportState = "SELECT State FROM Reports WHERE Rid=" + reportId;
 
-	connection.query(selectStrikes, function(err, rows) {
+	connection.query(selectReportState, function(err, rows) { 
 		if (err) {
-			console.log("Strike User: database error getting number of strikes: " + err);
+			console.log("StrikeUser: error getting report state: " + err);
 			return callback(-2);
 		}
-		else {
-			numStrikes = rows[0].NumberOfStrikes;
-			if (numStrikes < 2) {
-				var incrementStrikes = "UPDATE Users SET NumberOfStrikes=NumberOfStrikes+1 WHERE Uid=" + userId;
+		else if (rows[0].State < 2) {
+			var selectStrikes = "SELECT NumberOfStrikes FROM Users WHERE Uid=" + userId;
 
-				connection.query(incrementStrikes, function(err, rows) {
-					if (err) {
-						console.log("Strike User: database error updating number of strikes: " + err);
-						return callback(-2);
-					}
-					else {
-						var updateReportState = "UPDATE Reports SET State=2 WHERE Rid=" + reportId;
+			connection.query(selectStrikes, function(err, rows) {
+				if (err) {
+					console.log("Strike User: database error getting number of strikes: " + err);
+					return callback(-2);
+				}
+				else {
+					numStrikes = rows[0].NumberOfStrikes;
+					if (numStrikes < 2) {
+						var incrementStrikes = "UPDATE Users SET NumberOfStrikes=NumberOfStrikes+1 WHERE Uid=" + userId;
 
-						connection.query(updateReportState, function(err, rows) {
+						connection.query(incrementStrikes, function(err, rows) {
 							if (err) {
-								console.log("StrikeUser: Error update report state: " + err);
+								console.log("Strike User: database error updating number of strikes: " + err);
 								return callback(-2);
 							}
 							else {
-								console.log("Strike user successful");
-								return callback(0);
+								var updateReportState = "UPDATE Reports SET State=2 WHERE Rid=" + reportId;
+
+								connection.query(updateReportState, function(err, rows) {
+									if (err) {
+										console.log("StrikeUser: Error update report state: " + err);
+										return callback(-2);
+									}
+									else {
+										console.log("Strike user successful");
+										return callback(0);
+									}
+								});
 							}
 						});
 					}
-				});
-			}
-			else {
-				console.log("Stike user: user has 3 or more strikes - deleting user");
-				deleteUserHelper(userId, callback);
-			}
+					else {
+						console.log("Stike user: user has 3 or more strikes - deleting user");
+						deleteUserHelper(userId, callback);
+					}
+				}
+			});
+		}
+		else {
+			console.log("Report already accepted.");
+			return callback(0);
 		}
 	});
 }
