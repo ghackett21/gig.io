09703c4f545fb3b83d970c3589859452094cdab2
ghackett21 <ghackett@purdue.edu>
2017-04-27 22:28:58 -0400

    new access token

--- Server/helpers/dwollaClient.js
+++ Server/helpers/dwollaClient.js
@@ -14,7 +14,7 @@ var client = new dwolla.Client({
 /* Dwolla access token */
 // TODO - this expires after 60 minutes
 var appToken = new client.Token({
-    access_token: 'Y8AEX2A8qgJCDszPoThThRgA5wZRQmpMzTtAvsw1SfBEdGVDOJ',
+    access_token: 'srNHNegYUBBchOsJ0rz9SA7EhXkk7hWj1R1r8qcWMiLCK8x7gC',
     refresh_token: ""
 });
 
